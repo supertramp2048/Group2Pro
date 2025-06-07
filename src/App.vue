c0843b080347871177a92c90d71c068cc6bc5017
LongNguyen-ui <longkepro@gmail.com>
2025-06-07 14:47:03 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,15 @@ export default {
 
 };
 </script>
+<script setup>
+import { onMounted } from 'vue';
+import { useUserStore } from './stores/user';
 
+const user = useUserStore();
+
+onMounted(() => {
+  user.fetchSession();
+});
+
+</script>
 
